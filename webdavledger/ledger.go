859cdb5ee29d9a2fee94b63f5933117e3422c9d2
wangkui0508 <wangkui0508@hotmail.com>
2023-04-25 23:05:05 +0800

    Update ledger.go

--- webdavledger/ledger.go
+++ webdavledger/ledger.go
@@ -21,13 +21,13 @@ import (
 
 const (
 	RemainedPoints = byte(100) // key: RemainedPoints + uid, value: 8-byte int64
-	DeductPoints   = byte(101) // key: DeductPoints + uid + timestamp, value: 8-byte int64 + operation
-	AddPoints      = byte(102) // key: AddPoints + uid + timestamp, value: 8-byte int64 + txid
-	PasswordHash   = byte(103) // key: PasswordHash + 20-byte address, value: 32-byte passwd hash
-	SharedDir      = byte(104) // key: SharedDir + from-uid + to-uid + sha256(dir),
+	DeductPoints   = byte(102) // key: DeductPoints + uid + timestamp, value: 8-byte int64 + operation
+	AddPoints      = byte(104) // key: AddPoints + uid + timestamp, value: 8-byte int64 + txid
+	PasswordHash   = byte(106) // key: PasswordHash + 20-byte address, value: 32-byte passwd hash
+	SharedDir      = byte(108) // key: SharedDir + from-uid + to-uid + sha256(dir),
 	                           // value: 8-byte expiretime + dir
-	UserToId       = byte(105) // key: UserToId + 20-byte address, value: 8-byte uid
-	IdToUser       = byte(106) // key: IdToUser + uid, value: 20-byte address
+	UserToId       = byte(110) // key: UserToId + 20-byte address, value: 8-byte uid
+	IdToUser       = byte(112) // key: IdToUser + uid, value: 20-byte address
 	
 	PointsPerFileInfo = int64(30)
 	PointsOfMkdir     = int64(200)
